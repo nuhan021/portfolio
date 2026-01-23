4ef63990be538930ac4ef36dd4a7f34c8df4425e
MD Ali Nuhan Chowdhury <nuhan@Nuhans-MacBook-Pro.local>
2026-01-23 12:46:43 +0600

    Switch to HashRouter for reliable hosting

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/portfolio">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
