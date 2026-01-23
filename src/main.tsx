ead228748671b450873c7591351ff3d116828859
MD Ali Nuhan Chowdhury <nuhan@Nuhans-MacBook-Pro.local>
2026-01-23 12:44:41 +0600

    Fix routing with basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>
 );
